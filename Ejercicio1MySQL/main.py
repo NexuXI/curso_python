33985727d5df321cd9bf750e420492117b16a8fa
Alexandru <alexandrunneacsu@gmail.com>
2022-11-24 13:55:47 +0100

    Ejercicio crud con MySQL realizado.

---
+++ Ejercicio1MySQL/main.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import Proyecto.utils.conexion as con
+
+try:
+    conexion = con.get_mysql_conection()
+    with conexion:
+        cursor = conexion.cursor()
+        sentencia = 'Select nombre from personas'
+        cursor.execute(sentencia)
+        nombres = cursor.fetchall()
+        print(f"Todos los nombres de la tabla personas: {nombres}")
+        print("-------------------------------")
+        sentencia = "SELECT * FROM personas WHERE email LIKE '%@gmail.com';"
+        cursor.execute(sentencia)
+        gmail = cursor.fetchall()
+        print("Todos los datos de las personas con email acabado en @gmail.com: ", gmail)
+        sentencia = "SELECT * FROM personas WHERE email LIKE '%@gmail.com'"
+        cursor.execute(sentencia)
+        gmail = cursor.fetchall()
+        sentencia = "SELECT * FROM personas WHERE email not LIKE '%@gmail.com'"
+        cursor.execute(sentencia)
+        no_gmail = cursor.fetchall()
+        for persona in no_gmail:
+            index, nombre, apellidos, email = persona
+            nuevo_email_partes = email.split("@")
+            nuevo_email = (nuevo_email_partes[0] + "@gmail.com")
+            sentencia = f"UPDATE personas SET email=%s WHERE id=%s"
+            valores = (nuevo_email, index)
+            cursor.execute(sentencia, valores)
+        conexion.commit()
+        cursor = conexion.cursor()
+        sentencia = "SELECT * FROM personas WHERE email LIKE '%@gmail.com';"
+        cursor.execute(sentencia)
+        conexion.commit()
+except Exception as e:
+    conexion.rollback()
+    print(f'Ocurrió un error, se hizo rollback: {e}')

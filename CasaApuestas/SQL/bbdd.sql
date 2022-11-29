f7f5f0487fdfca0f58d41cd85591d8a1a19edabc
Alexandru <alexandrunneacsu@gmail.com>
2022-11-29 14:55:12 +0100

    Añadida la base de datos

--- CasaApuestas/SQL/bbdd.sql
+++ CasaApuestas/SQL/bbdd.sql
@@ -1,15 +1,3 @@
-select * from productos;
-
-INSERT INTO `curso_python`.`productos` (`id`, `nombre`, `precio`, `fecha_registro`) VALUES ('1', 'Prodcuto1', '100', '2022-10-13');
-
-Insert into productos (nombre, precio, fecha_registro) values(
-	"Producto3",
-    "200",
-    "2022-08-11"
-);
-
-select * from personas;
-
 create table gran_premio(
 	id int primary key auto_increment,
 	nombre varchar(45),
